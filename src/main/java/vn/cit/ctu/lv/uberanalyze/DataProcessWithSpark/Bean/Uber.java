c0f372e0f12ca92c6960da8095a0e51f9b41d622
PhamDuyKhang <khangy9a1@gmail.com>
2018-06-22 15:52:44 +0700

    add dependency

--- src/main/java/vn/cit/ctu/lv/uberanalyze/DataProcessWithSpark/Bean/Uber.java
+++ src/main/java/vn/cit/ctu/lv/uberanalyze/DataProcessWithSpark/Bean/Uber.java
@@ -1,5 +1,43 @@
 package vn.cit.ctu.lv.uberanalyze.DataProcessWithSpark.Bean;
 
 public class Uber {
-
+	private String dt;
+	private Double lat;
+	private Double lon;
+	private String base;
+	public Uber(String dt, Double lat, Double lon, String base) {
+		super();
+		this.dt = dt;
+		this.lat = lat;
+		this.lon = lon;
+		this.base = base;
+	}
+	public Uber() {
+		
+	}
+	public String getDt() {
+		return dt;
+	}
+	public void setDt(String dt) {
+		this.dt = dt;
+	}
+	public Double getLat() {
+		return lat;
+	}
+	public void setLat(Double lat) {
+		this.lat = lat;
+	}
+	public Double getLon() {
+		return lon;
+	}
+	public void setLon(Double lon) {
+		this.lon = lon;
+	}
+	public String getBase() {
+		return base;
+	}
+	public void setBase(String base) {
+		this.base = base;
+	}
+	
 }
---
+++ src/main/java/vn/cit/ctu/lv/uberanalyze/DataProcessWithSpark/ProcessData.java
@@ -0,0 +1,12 @@
+package vn.cit.ctu.lv.uberanalyze.DataProcessWithSpark;
+
+import org.apache.spark.streaming.StreamingContext;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+
+public class ProcessData {
+	public static void main(String[] args) {
+		
+	}
+
+}
